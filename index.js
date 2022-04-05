f100f2bed34e4e812c30cce5c6ded8e4bbc16881
Crystal Korth <crystalkorth@gmail.com>
2022-04-05 14:50:58 -0500

    added comments within the code

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ if (userAnswer === question.correctAnswer){
     console.log('Correct')
     }
 }
+//this command will pop up a new window with score
 window.alert ( `Your score is: ${userScore}`)
 }
 while (playAgain === true) {
 playGame()
+// I changed the yes and no to YES and NO since answers must be capitalized to count
 let userChoice = window.prompt ('Would you like to play the game again? Answer YES or NO.')
 if (userChoice === 'YES'){
     playAgain = true
